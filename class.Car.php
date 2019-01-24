be06870152a5db5fc2fd81c65205b65b8f3e3dc2
Nurmik <alex.nurmberg@gmail.com>
2019-01-24 15:11:14 +0200

    OOP

---
+++ class.Car.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex.nurmberg
+ * Date: 24.01.2019
+ * Time: 13:27
+ */
+
+class Car
+{
+    public $doors = 4;
+    public $windows = 10;
+}
+
+print_r(new car());
